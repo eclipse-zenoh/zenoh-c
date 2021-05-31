9c9b3c285932d05f3950313766bf3057c36022a9
Olivier Hécart <olivier.hecart@adlinktech.com>
2021-05-31 15:53:00 +0200

    Update zenoh-net rust scout API change

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -314,7 +314,7 @@ pub unsafe extern "C" fn zn_scout(
 
     let hellos = task::block_on(async move {
         let mut hs = std::vec::Vec::<Hello>::new();
-        let mut stream = zenoh::net::scout(what, ((*config).0).0.into());
+        let mut stream = zenoh::net::scout(what, ((*config).0).0.into()).await;
         let scout = async {
             while let Some(hello) = stream.next().await {
                 hs.push(hello)
