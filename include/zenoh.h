858b8025bec7152264e9cae9daa75e43c9afc24f
Pierre Avital <pierre.avital@me.com>
2021-11-30 12:16:39 +0100

    Add support for static linkage

--- include/zenoh.h
+++ include/zenoh.h
@@ -1,7 +1,13 @@
 #ifndef ZENOH_H
 #define ZENOH_H
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 #include "zenoh-concrete.h"
 #include "zenoh-commons.h"
 #include "zenoh-macros.h"
-
+#ifdef __cplusplus
+}
+#endif
 #endif
