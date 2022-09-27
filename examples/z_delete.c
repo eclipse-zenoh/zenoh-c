6d7910025b1fe8941c2d7de0fd05c397f69b481f
OlivierHecart <olivier.hecart@adlinktech.com>
2022-09-27 18:04:41 +0200

    Add z_delete example

---
+++ examples/z_delete.c
@@ -0,0 +1,51 @@
+//
+// Copyright (c) 2022 ZettaScale Technology
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+//
+#include <stdio.h>
+#include <string.h>
+
+#include "zenoh.h"
+
+int main(int argc, char **argv) {
+    char *keyexpr = "demo/example/zenoh-c-put";
+
+    if (argc > 1) keyexpr = argv[1];
+
+    z_owned_config_t config = z_config_default();
+    if (argc > 3) {
+        if (zc_config_insert_json(z_loan(config), Z_CONFIG_CONNECT_KEY, argv[3]) < 0) {
+            printf(
+                "Couldn't insert value `%s` in configuration at `%s`. This is likely because `%s` expects a "
+                "JSON-serialized list of strings\n",
+                argv[3], Z_CONFIG_CONNECT_KEY, Z_CONFIG_CONNECT_KEY);
+            exit(-1);
+        }
+    }
+
+    printf("Opening session...\n");
+    z_owned_session_t s = z_open(z_move(config));
+    if (!z_check(s)) {
+        printf("Unable to open session!\n");
+        exit(-1);
+    }
+
+    printf("Deleting resources matching '%s'...\n", keyexpr);
+    z_delete_options_t options = z_delete_options_default();
+    int res = z_delete(z_loan(s), z_keyexpr(keyexpr), &options);
+    if (!res) {
+        printf("Delete failed...\n");
+    }
+
+    z_close(z_move(s));
+    return 0;
+}
