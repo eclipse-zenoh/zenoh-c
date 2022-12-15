eb548e273b4cb98b95fa338e02c41d1d5d71a01b
Michael Ilyin <milyin@gmail.com>
2022-12-15 17:55:50 +0100

    128 bit align for MSVC added

--- include/zenoh.h
+++ include/zenoh.h
@@ -15,11 +15,24 @@ extern "C" {
 // 128-bit type used for alignment.
 #ifdef __SIZEOF_INT128__
 #define _z_u128 __uint128_t
+#elif _MSC_VER
+typedef __declspec(align(16)) struct _z_u128_aligned {
+    __int64 _0[2];
+} _z_u128_aligned;
+#define _z_u128 _z_u128_aligned
 #else
+// Let's hope that long double is 128 bit. If no, the assert below fires
 #define _z_u128 long double
 #endif
 
 static_assert(sizeof(_z_u128) == 16, "Size of _z_u128 must be 128 bit");
+
+typedef struct _z_u128_align_test {
+    char c;
+    _z_u128 u128;
+} _z_u128_align_test;
+
+static_assert(sizeof(_z_u128_align_test) == 32, "_z_u128 type must be aligned by 16-byte boundary");
 
 #include "zenoh_concrete.h"
 //
