8ba32c368853b8d128a30fea349a52c291ea7fdd
Michael Ilyin <milyin@gmail.com>
2023-03-05 14:45:13 +0100

    include_guard removed

--- cmake/helpers.cmake
+++ cmake/helpers.cmake
@@ -1,4 +1,22 @@
-include_guard()
+#
+# Copyright (c) 2023 ZettaScale Technology
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+# which is available at https://www.apache.org/licenses/LICENSE-2.0.
+#
+# SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+#
+# Contributors:
+#   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+#
+
+#
+# DO NOT add include_guard() to this file
+# It have to be reincluded multiple times on each 'include_project' call to restore
+# functions and macros definitions
+#
 
 #
 # Show VARIABLE = value on configuration stage
