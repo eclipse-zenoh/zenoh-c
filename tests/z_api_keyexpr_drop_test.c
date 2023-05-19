4a82affcf425f32bf59c04604d1b1b6b70ea90d1
Michael Ilyin <milyin@gmail.com>
2023-05-19 10:49:42 +0000

    test for subscriber, queriable added

--- tests/z_api_keyexpr_drop_test.c
+++ tests/z_api_keyexpr_drop_test.c
@@ -25,61 +25,58 @@ void test_publisher() {
     z_owned_session_t s = z_open(z_move(config));
     assert(z_check(s));
     char keyexpr[256];
-    strncpy(keyexpr, "foo/bar", 256);
+    strncpy(keyexpr, "foo/barr", 256);
     z_owned_publisher_t pub = z_declare_publisher(z_loan(s), z_keyexpr(keyexpr), NULL);
     strncpy(keyexpr, "baz/quax", 256);
     z_drop(z_move(pub));
     z_drop(z_move(s));
 }
 
-// void data_handler(const z_sample_t *sample, void *arg) {}
+void data_handler(const z_sample_t *sample, void *arg) {}
 
-// void test_pull_subscriber() {
-//     z_owned_config_t config = z_config_default();
-//     z_owned_session_t s = z_open(z_move(config));
-//     z_owned_closure_sample_t callback = z_closure(data_handler);
-//     z_owned_pull_subscriber_t sub = z_declare_pull_subscriber(z_loan(s), z_keyexpr(URL), z_move(callback), NULL);
-//     assert(z_check(sub));
-//     z_drop(z_move(sub));
-//     assert(!z_check(sub));
-//     z_drop(z_move(sub));
-//     assert(!z_check(sub));
-//     z_drop(z_move(s));
-// }
+void test_pull_subscriber() {
+    z_owned_config_t config = z_config_default();
+    z_owned_session_t s = z_open(z_move(config));
+    z_owned_closure_sample_t callback = z_closure(data_handler);
+    char keyexpr[256];
+    strncpy(keyexpr, "foo/bar", 256);
+    z_owned_pull_subscriber_t sub = z_declare_pull_subscriber(z_loan(s), z_keyexpr(keyexpr), z_move(callback), NULL);
+    strncpy(keyexpr, "baz/quax", 256);
+    z_drop(z_move(sub));
+    z_drop(z_move(s));
+}
 
-// void test_subscriber() {
-//     z_owned_config_t config = z_config_default();
-//     z_owned_session_t s = z_open(z_move(config));
-//     z_owned_closure_sample_t callback = z_closure(data_handler);
-//     z_owned_subscriber_t sub = z_declare_subscriber(z_loan(s), z_keyexpr(URL), z_move(callback), NULL);
-//     assert(z_check(sub));
-//     z_drop(z_move(sub));
-//     assert(!z_check(sub));
-//     z_drop(z_move(sub));
-//     assert(!z_check(sub));
-//     z_drop(z_move(s));
-// }
+void test_subscriber() {
+    z_owned_config_t config = z_config_default();
+    z_owned_session_t s = z_open(z_move(config));
+    z_owned_closure_sample_t callback = z_closure(data_handler);
+    char keyexpr[256];
+    strncpy(keyexpr, "foo/bar", 256);
+    z_owned_subscriber_t sub = z_declare_subscriber(z_loan(s), z_keyexpr(keyexpr), z_move(callback), NULL);
+    strncpy(keyexpr, "baz/quax", 256);
+    z_drop(z_move(sub));
+    z_drop(z_move(s));
+}
 
-// void query_handler(const z_query_t *query, void *context) {}
+void query_handler(const z_query_t *query, void *context) {}
 
-// void test_queryable() {
-//     z_owned_config_t config = z_config_default();
-//     z_owned_session_t s = z_open(z_move(config));
-//     z_owned_closure_query_t callback = z_closure(query_handler);
-//     z_owned_queryable_t queryable = z_declare_queryable(z_loan(s), z_keyexpr(URL), z_move(callback), NULL);
-//     assert(z_check(queryable));
-//     z_drop(z_move(queryable));
-//     assert(!z_check(queryable));
-//     z_drop(z_move(queryable));
-//     assert(!z_check(queryable));
-//     z_drop(z_move(s));
-// }
+void test_queryable() {
+    z_owned_config_t config = z_config_default();
+    z_owned_session_t s = z_open(z_move(config));
+    z_owned_closure_query_t callback = z_closure(query_handler);
+    char keyexpr[256];
+    strncpy(keyexpr, "foo/bar", 256);
+    z_owned_queryable_t queryable = z_declare_queryable(z_loan(s), z_keyexpr(keyexpr), z_move(callback), NULL);
+    strncpy(keyexpr, "baz/quax", 256);
+    z_drop(z_move(queryable));
+    z_drop(z_move(s));
+}
 
 int main(int argc, char **argv) {
     test_publisher();
-    // test_pull_subscriber();
-    // test_subscriber();
-    // test_queryable();
+    test_pull_subscriber();
+    test_subscriber();
+    test_queryable();
 
     return 0;
 }
