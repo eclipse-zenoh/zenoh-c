100a1773ef26618bcf9f75e031208c0178c54eb6
Steven Palma <imstevenpmwork@ieee.org>
2023-06-05 17:59:00 +0200

    Bump version to 0.7.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ from clang.cindex import Config
 project = 'zenoh-c'
 copyright = '2017, 2022 ZettaScale Technology'
 author = 'ZettaScale Zenoh team'
-release = '0.7.2-dev'
+release = '0.7.2.1'
 
 # -- General configuration ---------------------------------------------------
 master_doc = 'index'
