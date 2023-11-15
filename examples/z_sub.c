e24fed71d7d43473cc69db4f6b8a36cae8ecbcc7
Pierre Avital <pierre.avital@me.com>
2023-11-15 10:15:16 +0100

    correct typos

--- examples/z_sub.c
+++ examples/z_sub.c
@@ -13,7 +13,6 @@
 //
 #include <stdint.h>
 #include <stdio.h>
-#include <zenoh_macros.h>
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 #include <windows.h>
 #define sleep(x) Sleep(x * 1000)
