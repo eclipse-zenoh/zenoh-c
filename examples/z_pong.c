3137b6a7a7a7716989ef448c5e81b6365d56f3c0
OlivierHecart <olivier.hecart@adlinktech.com>
2023-11-29 16:35:25 +0100

    z_pong accepts a config file as argument

--- examples/z_pong.c
+++ examples/z_pong.c
@@ -1,4 +1,6 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <string.h>
+
 #include "zenoh.h"
 
 void callback(const z_sample_t* sample, void* context) {
@@ -18,9 +20,19 @@ void drop(void* context) {
     //  which makes passing a pointer to the stack safe as long as `sub` is dropped in a scope where `pub` is still
     //  valid.
 }
+struct args_t {
+    char* config_path;             // -c
+    uint8_t help_requested;        // -h
+};
+struct args_t parse_args(int argc, char** argv);
 
 int main(int argc, char** argv) {
-    z_owned_config_t config = z_config_default();
+    struct args_t args = parse_args(argc, argv);
+    if (args.help_requested) {
+        printf("-c (optional, string): the path to a configuration file for the session. If this option isn't passed, the default configuration will be used.\n");
+        return 1;
+    }
+    z_owned_config_t config = args.config_path ? zc_config_from_file(args.config_path) : z_config_default();
     z_owned_session_t session = z_open(z_move(config));
     z_keyexpr_t ping = z_keyexpr_unchecked("test/ping");
     z_keyexpr_t pong = z_keyexpr_unchecked("test/pong");
@@ -31,4 +43,27 @@ int main(int argc, char** argv) {
     }
     z_drop(z_move(sub));
     z_close(z_move(session));
+}
+
+char* getopt(int argc, char** argv, char option) {
+    for (int i = 0; i < argc; i++) {
+        size_t len = strlen(argv[i]);
+        if (len >= 2 && argv[i][0] == '-' && argv[i][1] == option) {
+            if (len > 2 && argv[i][2] == '=') {
+                return argv[i] + 3;
+            } else if (i + 1 < argc) {
+                return argv[i + 1];
+            }
+        }
+    }
+    return NULL;
+}
+
+struct args_t parse_args(int argc, char** argv) {
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "-h") == 0) {
+            return (struct args_t){.help_requested = 1};
+        }
+    }
+    return (struct args_t){.help_requested = 0, .config_path = getopt(argc, argv, 'c')};
 }
