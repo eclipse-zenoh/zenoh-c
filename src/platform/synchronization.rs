1da2801ee9f9eceb297b1a1e95dcc47e0ec182fc
Denis Biryukov <denis.biryukov@zettascale.tech>
2024-02-28 12:05:33 +0100

    set error values to correspond to the ones from GNU pthreads

--- src/platform/synchronization.rs
+++ src/platform/synchronization.rs
@@ -25,10 +25,11 @@ pub struct z_mutex_t(usize);
 impl_guarded_transmute!(z_mutex_t, ZMutexPtr);
 impl_guarded_transmute!(ZMutexPtr, z_mutex_t);
 
-const EBUSY: i8 = -1;
-const EINVAL: i8 = -2;
-const EAGAIN: i8 = -3;
-const EPOISON: i8 = -10;
+// try to use the same error codes as in GNU pthreads
+const EBUSY: i8 = 16;
+const EINVAL: i8 = 22;
+const EAGAIN: i8 = 11;
+const EPOISON: i8 = 22; // same as EINVAL
 
 #[no_mangle]
 #[allow(clippy::missing_safety_doc)]
