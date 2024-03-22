bd8c0eced849800f61914e941b21993933d303db
Oussama Teffahi <oussama.teffahi@zettascale.tech>
2024-03-22 17:03:24 +0100

    Update mode option help

--- examples/parse_args.h
+++ examples/parse_args.h
@@ -23,7 +23,7 @@
 #define COMMON_HELP \
     "\
         -c <CONFIG> (optional, string): The path to a configuration file for the session. If this option isn't passed, the default configuration will be used.\n\
-        -m <MODE> (optional, string, default='peer'): JSON-serialized string of the zenoh session mode. [possible values: 'peer', 'client', 'router']\n\
+        -m <MODE> (optional, string, default='peer'): The zenoh session mode. [possible values: peer, client, router]\n\
         -e <CONNECT> (optional, string): endpoint to connect to. Repeat option to pass multiple endpoints. If none are given, endpoints will be discovered through multicast-scouting if it is enabled.\n\
         -l <LISTEN> (optional, string): locator to listen on. Repeat option to pass multiple locators. If none are given, the default configuration will be used.\n\
         --no-multicast-scouting (optional): By default zenohd replies to multicast scouting messages for being discovered by peers and clients. This option disables this feature.\n\
