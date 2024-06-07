f6aa07d685122f85ff6cfb49afac220c5521b411
Alexander Bushnev <Alexander@Bushnev.pro>
2024-06-07 19:07:06 +0200

    Add missing properties to z_query_reply_options_t

--- tests/z_int_queryable_test.c
+++ tests/z_int_queryable_test.c
@@ -25,21 +25,40 @@ const char *const keyexpr = "test/key";
 const char *const values[] = {"test_value_1", "test_value_2", "test_value_3"};
 const size_t values_count = sizeof(values) / sizeof(values[0]);
 
+const uint32_t TEST_EID = 42;
+const uint64_t TEST_SN = 24;
+const uint64_t TEST_TS = 401706000;
+const uint8_t TEST_ID = 123;
+
 void query_handler(const z_loaned_query_t *query, void *context) {
     static int value_num = 0;
 
     z_view_string_t params;
     z_query_parameters(query, &params);
-    const z_loaned_value_t* payload_value = z_query_value(query);
+    const z_loaned_value_t *payload_value = z_query_value(query);
 
     z_query_reply_options_t options;
     z_query_reply_options_default(&options);
-    
+
+    z_id_t self_id;
+    self_id.id[0] = TEST_ID;
+
+    z_entity_global_id_t entity_global_id;
+    z_entity_global_id_new(&entity_global_id, &self_id, TEST_EID);
+    z_owned_source_info_t source_info;
+    z_source_info_new(&source_info, &entity_global_id, TEST_SN);
+
+    z_timestamp_t ts;
+    z_timestamp_new(&ts, &self_id, TEST_TS + value_num);
+
+    options.source_info = &source_info;
+    options.timestamp = &ts;
+
     z_owned_bytes_t payload;
     z_bytes_encode_from_string(&payload, values[value_num]);
 
     z_view_keyexpr_t reply_ke;
-    z_view_keyexpr_from_string(&reply_ke, (const char*)context);
+    z_view_keyexpr_from_string(&reply_ke, (const char *)context);
     z_query_reply(query, z_loan(reply_ke), z_move(payload), &options);
 
     if (++value_num == values_count) {
@@ -100,12 +119,53 @@ int run_get() {
         for (z_recv(z_loan(handler), &reply); z_check(reply); z_recv(z_loan(handler), &reply)) {
             assert(z_reply_is_ok(z_loan(reply)));
 
-            const z_loaned_sample_t* sample = z_reply_ok(z_loan(reply));
+            const z_loaned_sample_t *sample = z_reply_ok(z_loan(reply));
             z_owned_string_t payload_string;
             z_bytes_decode_into_string(z_sample_payload(sample), &payload_string);
             if (strncmp(values[val_num], z_string_data(z_loan(payload_string)), z_string_len(z_loan(payload_string)))) {
                 perror("Unexpected value received");
                 z_drop(z_move(payload_string));
+                exit(-1);
+            }
+
+            const z_loaned_source_info_t *source_info = z_sample_source_info(sample);
+            if (source_info == NULL) {
+                perror("Unexpected null source_info");
+                exit(-1);
+            }
+            const uint64_t sn = z_source_info_sn(source_info);
+            if (sn != TEST_SN) {
+                perror("Unexpected sn value");
+                exit(-1);
+            }
+            const z_entity_global_id_t id = z_source_info_id(source_info);
+            uint32_t eid = z_entity_global_id_eid(&id);
+            if (eid != TEST_EID) {
+                perror("Unexpected eid value");
+                exit(-1);
+            }
+
+            const z_timestamp_t *ts = z_sample_timestamp(sample);
+            if (ts == NULL) {
+                perror("Unexpected null timestamp");
+                exit(-1);
+            }
+            const uint64_t time = z_timestamp_npt64_time(ts);
+            if (time != TEST_TS + val_num) {
+                perror("Unexpected timestamp value");
+                exit(-1);
+            }
+
+            z_id_t ts_id = z_timestamp_id(ts);
+            z_id_t gloabl_id = z_entity_global_id_zid(&id);
+
+            if (memcmp(ts_id.id, gloabl_id.id, sizeof(ts_id.id)) != 0) {
+                perror("Timestamp id and global id differ");
+                exit(-1);
+            }
+
+            if (ts_id.id[0] != TEST_ID) {
+                perror("Unexpected id value");
                 exit(-1);
             }
 
