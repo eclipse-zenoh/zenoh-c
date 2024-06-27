60da9636a78d79203c779bc27672d847a686d235
Michael Ilyin <milyin@gmail.com>
2024-06-27 14:44:27 +0200

    sample channel updated

--- src/closures/sample_channel.rs
+++ src/closures/sample_channel.rs
@@ -3,7 +3,7 @@ use crate::{
         unwrap_ref_unchecked, Inplace, TransmuteFromHandle, TransmuteIntoHandle, TransmuteRef,
         TransmuteUninitPtr,
     },
-    transmute2::{RustTypeRef, RustTypeRefUninit},
+    transmute2::{LoanedCTypeRef, RustTypeRef, RustTypeRefUninit},
     z_loaned_sample_t, z_owned_closure_sample_t, z_owned_sample_t,
 };
 use libc::c_void;
@@ -15,35 +15,29 @@ use zenoh::{
 
 pub use crate::opaque_types::z_loaned_fifo_handler_sample_t;
 pub use crate::opaque_types::z_owned_fifo_handler_sample_t;
-
-decl_transmute_owned!(
-    Option<flume::Receiver<Sample>>,
-    z_owned_fifo_handler_sample_t
-);
-decl_transmute_handle!(flume::Receiver<Sample>, z_loaned_fifo_handler_sample_t);
-validate_equivalence!(
-    z_owned_fifo_handler_sample_t,
-    z_loaned_fifo_handler_sample_t
+decl_c_type!(
+    owned(z_owned_fifo_handler_sample_t, Option<flume::Receiver<Sample>>),
+    loaned(z_loaned_fifo_handler_sample_t, flume::Receiver<Sample>)
 );
 
 /// Drops the handler and resets it to a gravestone state.
 #[no_mangle]
 pub extern "C" fn z_fifo_handler_sample_drop(this: &mut z_owned_fifo_handler_sample_t) {
-    Inplace::drop(this.transmute_mut());
+    *this.as_rust_type_mut() = None;
 }
 
 /// Constructs a handler in gravestone state.
 #[no_mangle]
 pub extern "C" fn z_fifo_handler_sample_null(
-    this: *mut MaybeUninit<z_owned_fifo_handler_sample_t>,
+    this: &mut MaybeUninit<z_owned_fifo_handler_sample_t>,
 ) {
-    Inplace::empty(this.transmute_uninit_ptr());
+    this.as_rust_type_mut_uninit().write(None);
 }
 
 /// Returns ``true`` if handler is valid, ``false`` if it is in gravestone state.
 #[no_mangle]
 pub extern "C" fn z_fifo_handler_sample_check(this: &z_owned_fifo_handler_sample_t) -> bool {
-    this.transmute_ref().is_some()
+    this.as_rust_type_ref().is_some()
 }
 
 extern "C" fn __z_handler_sample_send(sample: &z_loaned_sample_t, context: *mut c_void) {
@@ -66,15 +60,15 @@ extern "C" fn __z_handler_sample_drop(context: *mut c_void) {
 #[no_mangle]
 #[allow(clippy::missing_safety_doc)]
 pub unsafe extern "C" fn z_fifo_channel_sample_new(
-    callback: *mut MaybeUninit<z_owned_closure_sample_t>,
-    handler: *mut MaybeUninit<z_owned_fifo_handler_sample_t>,
+    callback: &mut MaybeUninit<z_owned_closure_sample_t>,
+    handler: &mut MaybeUninit<z_owned_fifo_handler_sample_t>,
     capacity: usize,
 ) {
     let fifo = handlers::FifoChannel::new(capacity);
     let (cb, h) = fifo.into_handler();
     let cb_ptr = Box::into_raw(Box::new(cb)) as *mut libc::c_void;
-    Inplace::init(handler.transmute_uninit_ptr(), Some(h));
-    (*callback).write(z_owned_closure_sample_t {
+    handler.as_rust_type_mut_uninit().write(Some(h));
+    callback.write(z_owned_closure_sample_t {
         call: Some(__z_handler_sample_send),
         context: cb_ptr,
         drop: Some(__z_handler_sample_drop),
@@ -83,10 +77,14 @@ pub unsafe extern "C" fn z_fifo_channel_sample_new(
 
 /// Borrows handler.
 #[no_mangle]
-pub extern "C" fn z_fifo_handler_sample_loan(
+#[allow(clippy::missing_safety_doc)]
+pub unsafe extern "C" fn z_fifo_handler_sample_loan(
     this: &z_owned_fifo_handler_sample_t,
 ) -> &z_loaned_fifo_handler_sample_t {
-    unwrap_ref_unchecked(this.transmute_ref()).transmute_handle()
+    this.as_rust_type_ref()
+        .as_ref()
+        .unwrap_unchecked()
+        .as_loaned_ctype_ref()
 }
 
 /// Returns sample from the fifo buffer. If there are no more pending replies will block until next sample is received, or until
@@ -97,7 +95,7 @@ pub extern "C" fn z_fifo_handler_sample_recv(
     this: &z_loaned_fifo_handler_sample_t,
     sample: &mut MaybeUninit<z_owned_sample_t>,
 ) -> bool {
-    match this.transmute_ref().recv() {
+    match this.as_rust_type_ref().recv() {
         Ok(q) => {
             sample.as_rust_type_mut_uninit().write(Some(q));
             true
@@ -116,7 +114,7 @@ pub extern "C" fn z_fifo_handler_sample_try_recv(
     this: &z_loaned_fifo_handler_sample_t,
     sample: &mut MaybeUninit<z_owned_sample_t>,
 ) -> bool {
-    match this.transmute_ref().try_recv() {
+    match this.as_rust_type_ref().try_recv() {
         Ok(q) => {
             sample.as_rust_type_mut_uninit().write(Some(q));
             true
