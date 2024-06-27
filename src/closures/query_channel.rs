db923fd86407d5818c9d730f8763a1a768afe4a1
Michael Ilyin <milyin@gmail.com>
2024-06-27 12:39:46 +0200

    query channel

--- src/closures/query_channel.rs
+++ src/closures/query_channel.rs
@@ -17,7 +17,7 @@ use crate::{
         unwrap_ref_unchecked, Inplace, TransmuteFromHandle, TransmuteIntoHandle, TransmuteRef,
         TransmuteUninitPtr,
     },
-    transmute2::{RustTypeRef, RustTypeRefUninit},
+    transmute2::{LoanedCTypeRef, RustTypeRef, RustTypeRefUninit},
     z_loaned_query_t, z_owned_closure_query_t, z_owned_query_t,
 };
 use libc::c_void;
@@ -29,27 +29,27 @@ use zenoh::{
 
 pub use crate::opaque_types::z_loaned_fifo_handler_query_t;
 pub use crate::opaque_types::z_owned_fifo_handler_query_t;
-
-decl_transmute_owned!(Option<flume::Receiver<Query>>, z_owned_fifo_handler_query_t);
-decl_transmute_handle!(flume::Receiver<Query>, z_loaned_fifo_handler_query_t);
-validate_equivalence!(z_owned_fifo_handler_query_t, z_loaned_fifo_handler_query_t);
+decl_c_type!(
+    owned(z_owned_fifo_handler_query_t, Option<flume::Receiver<Query>>),
+    loaned(z_loaned_fifo_handler_query_t, flume::Receiver<Query>)
+);
 
 /// Drops the handler and resets it to a gravestone state.
 #[no_mangle]
 pub extern "C" fn z_fifo_handler_query_drop(this: &mut z_owned_fifo_handler_query_t) {
-    Inplace::drop(this.transmute_mut());
+    *this.as_rust_type_mut() = None;
 }
 
 /// Constructs a handler in gravestone state.
 #[no_mangle]
-pub extern "C" fn z_fifo_handler_query_null(this: *mut MaybeUninit<z_owned_fifo_handler_query_t>) {
-    Inplace::empty(this.transmute_uninit_ptr());
+pub extern "C" fn z_fifo_handler_query_null(this: &mut MaybeUninit<z_owned_fifo_handler_query_t>) {
+    this.as_rust_type_mut_uninit().write(None);
 }
 
 /// Returns ``true`` if handler is valid, ``false`` if it is in gravestone state.
 #[no_mangle]
 pub extern "C" fn z_fifo_handler_query_check(this: &z_owned_fifo_handler_query_t) -> bool {
-    this.transmute_ref().is_some()
+    this.as_rust_type_ref().is_some()
 }
 
 extern "C" fn __z_handler_query_send(query: &z_loaned_query_t, context: *mut c_void) {
@@ -72,15 +72,15 @@ extern "C" fn __z_handler_query_drop(context: *mut c_void) {
 #[no_mangle]
 #[allow(clippy::missing_safety_doc)]
 pub unsafe extern "C" fn z_fifo_channel_query_new(
-    callback: *mut MaybeUninit<z_owned_closure_query_t>,
-    handler: *mut MaybeUninit<z_owned_fifo_handler_query_t>,
+    callback: &mut MaybeUninit<z_owned_closure_query_t>,
+    handler: &mut MaybeUninit<z_owned_fifo_handler_query_t>,
     capacity: usize,
 ) {
     let fifo = handlers::FifoChannel::new(capacity);
     let (cb, h) = fifo.into_handler();
     let cb_ptr = Box::into_raw(Box::new(cb)) as *mut libc::c_void;
-    Inplace::init(handler.transmute_uninit_ptr(), Some(h));
-    (*callback).write(z_owned_closure_query_t {
+    handler.as_rust_type_mut_uninit().write(Some(h));
+    callback.write(z_owned_closure_query_t {
         call: Some(__z_handler_query_send),
         context: cb_ptr,
         drop: Some(__z_handler_query_drop),
@@ -89,10 +89,14 @@ pub unsafe extern "C" fn z_fifo_channel_query_new(
 
 /// Borrows handler.
 #[no_mangle]
-pub extern "C" fn z_fifo_handler_query_loan(
+#[allow(clippy::missing_safety_doc)]
+pub unsafe extern "C" fn z_fifo_handler_query_loan(
     this: &z_owned_fifo_handler_query_t,
 ) -> &z_loaned_fifo_handler_query_t {
-    unwrap_ref_unchecked(this.transmute_ref()).transmute_handle()
+    this.as_rust_type_ref()
+        .as_ref()
+        .unwrap_unchecked()
+        .as_loaned_ctype_ref()
 }
 
 /// Returns query from the fifo buffer. If there are no more pending queries will block until next query is received, or until
@@ -103,7 +107,7 @@ pub extern "C" fn z_fifo_handler_query_recv(
     this: &z_loaned_fifo_handler_query_t,
     query: &mut MaybeUninit<z_owned_query_t>,
 ) -> bool {
-    match this.transmute_ref().recv() {
+    match this.as_rust_type_ref().recv() {
         Ok(q) => {
             query.as_rust_type_mut_uninit().write(Some(q));
             true
@@ -122,7 +126,7 @@ pub extern "C" fn z_fifo_handler_query_try_recv(
     this: &z_loaned_fifo_handler_query_t,
     query: &mut MaybeUninit<z_owned_query_t>,
 ) -> bool {
-    match this.transmute_ref().try_recv() {
+    match this.as_rust_type_ref().try_recv() {
         Ok(q) => {
             query.as_rust_type_mut_uninit().write(Some(q));
             true
