972e337f876f0c059eab549a28854615383f1ac5
Denis Biryukov <denis.biryukov@zettascale.tech>
2024-08-08 14:05:38 +0200

    fix initializer

--- tests/z_api_payload_test.c
+++ tests/z_api_payload_test.c
@@ -101,7 +101,7 @@ void test_writer() {
 
 void test_bounded(void) {
     uint32_t data[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    uint32_t data_out[10] = {};
+    uint32_t data_out[10] = {0};
 
     z_owned_bytes_t payload;
     z_bytes_empty(&payload);
