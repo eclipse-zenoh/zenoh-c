a874982355b2809a48401d5d36550223079c44d4
DenisBiryukov91 <155981813+DenisBiryukov91@users.noreply.github.com>
2024-10-16 13:59:51 +0200

    impose utf-8 correctness check when serializing strings; (#771)
    add serialization functions for substrings;

--- src/result.rs
+++ src/result.rs
@@ -26,6 +26,7 @@ pub const Z_ENULL: z_result_t = -5;
 pub const Z_EUNAVAILABLE: z_result_t = -6;
 pub const Z_EDESERIALIZE: z_result_t = -7;
 pub const Z_ESESSION_CLOSED: z_result_t = -8;
+pub const Z_EUTF8: z_result_t = -9;
 // negative pthread error codes (due to convention to return negative values on error)
 pub const Z_EBUSY_MUTEX: z_result_t = -16;
 pub const Z_EINVAL_MUTEX: z_result_t = -22;
