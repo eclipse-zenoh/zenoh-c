3669fe86f3d680fddd242b7d68d055349dfd89bd
Diogo Mendes Matsubara <diogo.matsubara@zettascale.tech>
2024-10-24 12:26:40 +0200

    fix: Add cmake toolchain files for all targets

--- TC-x86_64-unknown-linux-gnu.cmake
+++ TC-x86_64-unknown-linux-gnu.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 set(ZENOHC_CUSTOM_TARGET x86_64-unknown-linux-gnu)
-set(CMAKE_C_COMPILER x86_64-unknown-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER x86_64-unknown-linux-gnu-g++)
+set(CMAKE_C_COMPILER x86_64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER x86_64-linux-gnu-g++)
