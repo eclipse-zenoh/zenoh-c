0b97c60ddea3beca93dff5c77702fca5eaed6f18
DenisBiryukov91 <155981813+DenisBiryukov91@users.noreply.github.com>
2024-10-25 16:30:23 +0200

    Add cross-compile tests to ci and fix library names (#790)
    * add sample toolchains and fix library names
    
    * add cross-compilation to ci
    
    * update linkers in config.toml
    
    * add cross-compilation test for linux

---
+++ ci/toolchains/TC-arm-unknown-linux-gnueabi.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(ZENOHC_CUSTOM_TARGET arm-unknown-linux-gnueabi)
+set(CMAKE_C_COMPILER arm-linux-gnueabi-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabi-g++)
