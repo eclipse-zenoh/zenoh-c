357e9073213e20153c8e3fd52de73a49187baa12
Denis Biryukov <denis.biryukov@zettascale.tech>
2024-10-28 10:40:27 +0100

    fix deb/rpm architecture; move static lib to dev package;

--- install/cpack_project_config.cmake
+++ install/cpack_project_config.cmake
@@ -1,6 +1,5 @@
 if(CPACK_GENERATOR MATCHES "DEB")
     # DEB package
-    message(STATUS "Configure DEB packaging for Linux ${DEBARCH}")
     set(CPACK_DEBIAN_PACKAGE_MAINTAINER "ZettaScale Zenoh Team, <zenoh@zettascale.tech>")
     set(CPACK_DEB_COMPONENT_INSTALL ON)
     set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
@@ -14,11 +13,6 @@ if(CPACK_GENERATOR MATCHES "RPM")
   # RPM package
   # rpmbuild should be installed
   # apt install rpm
-  if(NOT RPMARCH)
-    set(RPMARCH ${CMAKE_SYSTEM_PROCESSOR})
-  endif()
-  message(STATUS "Configure RPM packaging for Linux ${RPMARCH}")
-  set(CPACK_RPM_PACKAGE_ARCHITECTURE ${RPMARCH})
   set(CPACK_RPM_COMPONENT_INSTALL ON)
   set(CPACK_RPM_FILE_NAME RPM-DEFAULT)
   set(CPACK_RPM_LIB_PACKAGE_NAME ${CPACK_PACKAGE_NAME}) # avoid "-lib" suffix for "lib" package
