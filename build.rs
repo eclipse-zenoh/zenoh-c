7488c3d4c7db9c27a236f92cd0c92f4d22739c31
Michael Ilyin <milyin@gmail.com>
2025-03-21 12:03:48 +0100

    add buildrs directory to changed

--- build.rs
+++ build.rs
@@ -11,6 +11,7 @@ fn main() {
     buildrs::opaque_types_generator::generate_opaque_types();
     buildrs::cbindgen_generator::generate_c_headers();
     println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=buildrs");
     println!("cargo:rerun-if-changed=src");
     println!("cargo:rerun-if-changed=splitguide.yaml");
     println!("cargo:rerun-if-changed=cbindgen.toml");
