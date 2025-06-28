76c7d0b8bd46b2b8aa44ea33bc161ad4f97f9f90
Michael Ilyin <milyin@gmail.com>
2025-06-28 16:42:32 +0200

    c-related part stripped

--- build.rs
+++ build.rs
@@ -16,18 +16,8 @@ pub fn get_out_rs_path() -> std::path::PathBuf {
 
 fn main() {
     buildrs::opaque_types_generator::generate_opaque_types();
-    buildrs::cbindgen_generator::generate_c_headers();
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=buildrs");
     println!("cargo:rerun-if-changed=src");
-    println!("cargo:rerun-if-changed=splitguide.yaml");
-    println!("cargo:rerun-if-changed=cbindgen.toml");
     println!("cargo:rerun-if-changed=build-resources");
-    println!("cargo:rerun-if-changed=include");
-    if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("linux") {
-        let name = std::env::var("CARGO_PKG_NAME").unwrap();
-        // Create the shared library name by removing hyphens from the pkg_name
-        let soname = format!("lib{}.so", name.replace('-', ""));
-        println!("cargo:rustc-cdylib-link-arg=-Wl,-soname,{}", soname);
-    }
 }
