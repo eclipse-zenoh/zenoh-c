f97aca858b9dccbd8cbf566c1307320164760e0a
Michael Ilyin <milyin@gmail.com>
2025-07-31 18:03:48 +0200

    unused file removed

--- buildrs/csbindgen_generator.rs
+++
@@ -1,9 +0,0 @@
-pub fn generate_cs_bindings() {
-    csbindgen::Builder::default()
-        .csharp_dll_name("zenohc")
-        .csharp_namespace("Zenoh")
-        .csharp_class_name("ZenohFFI")
-        .input_extern_file("src/lib.rs")
-        .generate_csharp_file("csoutput/zenoh.cs")
-        .unwrap();
-}
