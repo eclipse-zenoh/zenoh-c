4e72dc50c2d41c6635b5ee6d99d8add71e0c0a89
Michael Ilyin <milyin@gmail.com>
2025-07-31 14:57:49 +0200

    export zenoh features constant

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,11 @@
 
 pub const PREBINDGEN_OUT_DIR: &str = prebindgen_proc_macro::prebindgen_out_dir!();
 
+// Just reexport zenoh features list for simplicity, zenoh-ffi just mirrors them
+// It it becomes necessary to know the own zenoh-ffi features, it can be replaced with
+// the own FEATURES constant, using the same `zenoh_util::concat_enabled_features!` macro.
+pub const ZENOH_FEATURES: &str = zenoh::FEATURES;
+
 use std::{cmp::min, slice};
 
 use libc::c_void;
