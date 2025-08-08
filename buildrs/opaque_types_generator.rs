0b30cf5b967c86d1bc6277930e4b9ec2133390d5
Michael Ilyin <milyin@gmail.com>
2025-08-08 23:54:48 +0200

    dest_lock.sav fix

--- buildrs/opaque_types_generator.rs
+++ buildrs/opaque_types_generator.rs
@@ -159,8 +159,9 @@ fn copy_cargo_files_to_out_dir(target: &str) -> PathBuf {
         let dest_lock = out_dir.join("Cargo.lock");
         std::fs::copy(&source_lock, &dest_lock)
             .unwrap_or_else(|_| panic!("Failed to copy Cargo.lock to {}", dest_lock.display()));
-        std::fs::copy(&source_lock, dest_lock.join(".sav"))
-            .unwrap_or_else(|_| panic!("Failed to copy Cargo.lock to {}", dest_lock.display()));
+        let dest_lock_sav = dest_lock.with_extension("sav");
+        std::fs::copy(&source_lock, &dest_lock_sav)
+            .unwrap_or_else(|_| panic!("Failed to copy Cargo.lock to {}", dest_lock_sav.display()));
     }
     
     dest_manifest
