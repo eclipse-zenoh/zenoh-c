eff0dfe0cbd1105a0ceb34861fb5ed08601298ef
Michael Ilyin <milyin@gmail.com>
2025-08-15 14:29:51 +0200

    zenoh-ffi-opaque-types compiles

--- zenoh-ffi-opaque-types/build.rs
+++ zenoh-ffi-opaque-types/build.rs
@@ -5,9 +5,6 @@ use std::{
 
 use regex::Regex;
 
-use super::common_helpers::{features, split_type_name};
-use crate::{get_build_rs_path, get_out_dir};
-
 // This script copies it's own project (with necessary modifications, 
 // e.g. without the build.rs itself) to the root of target directory 
 // (on the same level as debug and release directories)
@@ -332,3 +329,41 @@ fn get_opaque_type_docs() -> HashMap<String, Vec<String>> {
     }
     res
 }
+
+use std::collections::BTreeSet;
+
+pub fn split_type_name(type_name: &str) -> (&str, Option<&str>, &str, &str) {
+    let mut split = type_name.split('_');
+    let prefix = split
+        .next()
+        .unwrap_or_else(|| panic!("Fist '_' not found in type name: {type_name}"));
+    let cat = split
+        .next()
+        .unwrap_or_else(|| panic!("Second '_' not found in type name: {type_name}"));
+    let category = if cat != "owned" && cat != "loaned" && cat != "moved" {
+        None
+    } else {
+        Some(cat)
+    };
+    let postfix = split.next_back().expect("Type should end with '_t'");
+    let prefix_cat_len = prefix.len() + 1 + category.map(|c| c.len() + 1).unwrap_or(0);
+    let semantic = &type_name[prefix_cat_len..type_name.len() - postfix.len() - 1];
+    (prefix, category, semantic, postfix)
+}
+
+pub fn features() -> BTreeSet<&'static str> {
+    zenoh::FEATURES.split(" zenoh/").collect()
+}
+
+pub fn get_build_rs_path() -> std::path::PathBuf {
+    let file_path = file!();
+    let mut path_buf = std::path::PathBuf::new();
+    path_buf.push(file_path);
+    path_buf.parent().unwrap().to_path_buf()
+}
+
+pub fn get_out_dir() -> std::path::PathBuf {
+    let out_dir = std::env::var_os("OUT_DIR").unwrap();
+    std::path::Path::new(&out_dir).to_path_buf()
+}
+
