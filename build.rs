991dd6999dea8f0e2ca853f589b05a3535f3d284
Michael Ilyin <milyin@gmail.com>
2025-08-17 15:05:53 +0200

    cleanup

--- build.rs
+++ build.rs
@@ -1,41 +1,4 @@
-use std::env;
-
-use crate::buildrs::opaque_types_generator::generate_opaque_types;
-mod buildrs;
-
-pub fn get_build_rs_path() -> std::path::PathBuf {
-    let file_path = file!();
-    let mut path_buf = std::path::PathBuf::new();
-    path_buf.push(file_path);
-    path_buf.parent().unwrap().to_path_buf()
-}
-
-pub fn get_out_dir() -> std::path::PathBuf {
-    let out_dir = env::var_os("OUT_DIR").unwrap();
-    std::path::Path::new(&out_dir).to_path_buf()
-}
-
 fn main() {
     // Initialize the directory for collecting API for dependent crates
     prebindgen::init_prebindgen_out_dir();
-
-    // Copy Cargo.lock from CARGO_LOCK environment variable
-    let cargo_lock_path = zenoh_ffi_workspace::get_cargo_lock_path();
-    let path_out = get_out_dir().join("opaque_types.rs");
-    let target = std::env::var("TARGET").unwrap();
-    generate_opaque_types(&path_out, &target, &cargo_lock_path);
-    prebindgen::trace!(
-        "Generated opaque types for TARGET={} : {}",
-        target,
-        path_out.display()
-    );
-
-    // Pass list of zenoh features enabled to build.rs of dependent crates
-    // in the variable DEP_ZENOH_FFI_FEATURES
-    println!("cargo:FEATURES={}", zenoh::FEATURES);
-
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=buildrs");
-    println!("cargo:rerun-if-changed=src");
-    println!("cargo:rerun-if-changed=build-resources");
 }
--- buildrs/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod common_helpers;
-pub mod opaque_types_generator;
