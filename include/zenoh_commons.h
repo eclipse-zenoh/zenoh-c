eclipse-zenoh/zenoh-c

commit 16cba0760b2305b7cacd1e7abe15765237d5025d

feat: owned queries now supported, allowing responses out of callback (#213)

include/zenoh_commons.h
@@ -631,6 +631,19 @@ typedef struct z_put_options_t {
   enum z_priority_t priority;
 } z_put_options_t;
 /**
+ * Owned variant of a Query received by a Queryable.
+ *
+ * You may construct it by `z_query_clone`-ing a loaned query.
+ * When the last `z_owned_query_t` corresponding to a query is destroyed, or the callback that produced the query cloned to build them returns,
+ * the query will receive its termination signal.
+ *
+ * Holding onto an `z_owned_query_t` for too long (10s by default, can be set in `z_get`'s options) will trigger a timeout error
+ * to be sent to the querier by the infrastructure, and new responses to the outdated query will be silently dropped.
+ */
+typedef struct z_owned_query_t {
+  void *_0;
+} z_owned_query_t;
+/**
  * Represents the set of options that can be applied to a query reply,
  * sent via :c:func:`z_query_reply`.
  *
@@ -1415,6 +1428,20 @@ int8_t z_put(struct z_session_t session,
  */
 ZENOHC_API struct z_put_options_t z_put_options_default(void);
 /**
+ * Returns `false` if `this` is in a gravestone state, `true` otherwise.
+ *
+ * This function may not be called with the null pointer, but can be called with the gravestone value.
+ */
+ZENOHC_API
+bool z_query_check(const struct z_owned_query_t *this_);
+/**
+ * Clones the query, allowing to keep it in an "open" state past the callback's return.
+ *
+ * This operation is infallible, but may return a gravestone value if `query` itself was a gravestone value (which cannot be the case in a callback).
+ */
+ZENOHC_API
+struct z_owned_query_t z_query_clone(const struct z_query_t *query);
+/**
  * Automatic query consolidation strategy selection.
  *
  * A query consolidation strategy will automatically be selected depending the query selector.
@@ -1442,9 +1469,27 @@ ZENOHC_API struct z_query_consolidation_t z_query_consolidation_monotonic(void);
  */
 ZENOHC_API struct z_query_consolidation_t z_query_consolidation_none(void);
 /**
+ * Destroys the query, setting `this` to its gravestone value to prevent double-frees.
+ *
+ * This function may not be called with the null pointer, but can be called with the gravestone value.
+ */
+ZENOHC_API
+void z_query_drop(struct z_owned_query_t *this_);
+/**
  * Get a query's key by aliasing it.
  */
 ZENOHC_API struct z_keyexpr_t z_query_keyexpr(const struct z_query_t *query);
+/**
+ * Aliases the query.
+ *
+ * This function may not be called with the null pointer, but can be called with the gravestone value.
+ */
+ZENOHC_API
+struct z_query_t z_query_loan(const struct z_owned_query_t *this_);
+/**
+ * The gravestone value of `z_owned_query_t`.
+ */
+ZENOHC_API struct z_owned_query_t z_query_null(void);
 /**
  * Get a query's `value selector <https://github.com/eclipse-zenoh/roadmap/tree/main/rfcs/ALL/Selectors>`_ by aliasing it.
  */

src/queryable.rs
@@ -17,7 +17,7 @@ use crate::{
     LOG_INVALID_SESSION,
 };
 use libc::c_void;
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
 use zenoh::prelude::SessionDeclarations;
 use zenoh::{
     prelude::{Sample, SplitBuffer},
@@ -76,14 +76,75 @@ pub extern "C" fn z_queryable_null() -> z_owned_queryable_t {
     z_owned_queryable_t::null()
 }
 
-/// Structs received by a Queryable.
+/// Loaned variant of a Query received by a Queryable.
+///
+/// Queries are atomically reference-counted, letting you extract them from the callback that handed them to you by cloning.
+/// `z_query_t`'s are valid as long as at least one corresponding `z_owned_query_t` exists, including the one owned by Zenoh until the callback returns.
 #[allow(non_camel_case_types)]
 #[repr(C)]
 pub struct z_query_t(*mut c_void);
+/// Owned variant of a Query received by a Queryable.
+///
+/// You may construct it by `z_query_clone`-ing a loaned query.
+/// When the last `z_owned_query_t` corresponding to a query is destroyed, or the callback that produced the query cloned to build them returns,
+/// the query will receive its termination signal.
+///
+/// Holding onto an `z_owned_query_t` for too long (10s by default, can be set in `z_get`'s options) will trigger a timeout error
+/// to be sent to the querier by the infrastructure, and new responses to the outdated query will be silently dropped.
+#[allow(non_camel_case_types)]
+#[repr(C)]
+pub struct z_owned_query_t(*mut c_void);
 impl Deref for z_query_t {
-    type Target = Query;
+    type Target = Option<Query>;
     fn deref(&self) -> &Self::Target {
         unsafe { &*(self.0 as *const _) }
+    }
+}
+impl Deref for z_owned_query_t {
+    type Target = Option<Query>;
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*(self.0 as *const _) }
+    }
+}
+impl DerefMut for z_owned_query_t {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *(self.0 as *mut _) }
+    }
+}
+/// The gravestone value of `z_owned_query_t`.
+#[no_mangle]
+pub extern "C" fn z_query_null() -> z_owned_query_t {
+    unsafe { core::mem::transmute(None::<Query>) }
+}
+/// Returns `false` if `this` is in a gravestone state, `true` otherwise.
+///
+/// This function may not be called with the null pointer, but can be called with the gravestone value.
+#[no_mangle]
+pub extern "C" fn z_query_check(this: &z_owned_query_t) -> bool {
+    this.is_some()
+}
+/// Aliases the query.
+///
+/// This function may not be called with the null pointer, but can be called with the gravestone value.
+#[no_mangle]
+pub extern "C" fn z_query_loan(this: &z_owned_query_t) -> z_query_t {
+    unsafe { core::mem::transmute_copy(this) }
+}
+/// Destroys the query, setting `this` to its gravestone value to prevent double-frees.
+///
+/// This function may not be called with the null pointer, but can be called with the gravestone value.
+#[no_mangle]
+pub extern "C" fn z_query_drop(this: &mut z_owned_query_t) {
+    let _: Option<Query> = this.take();
+}
+/// Clones the query, allowing to keep it in an "open" state past the callback's return.
+///
+/// This operation is infallible, but may return a gravestone value if `query` itself was a gravestone value (which cannot be the case in a callback).
+#[no_mangle]
+pub extern "C" fn z_query_clone(query: Option<&z_query_t>) -> z_owned_query_t {
+    match query {
+        Some(query) => unsafe { core::mem::transmute(query.as_ref().map(|q| q.clone())) },
+        None => z_query_null(),
     }
 }
 
@@ -210,6 +271,10 @@ pub unsafe extern "C" fn z_query_reply(
     len: usize,
     options: Option<&z_query_reply_options_t>,
 ) -> i8 {
+    let Some(query) = query.as_ref() else {
+        log::error!("Called `z_query_reply` with invalidated `query`");
+        return i8::MIN;
+    };
     if let Some(key) = &*key {
         let mut s = Sample::new(
             key.clone().into_owned(),
@@ -232,6 +297,9 @@ pub unsafe extern "C" fn z_query_reply(
 #[allow(clippy::missing_safety_doc)]
 #[no_mangle]
 pub extern "C" fn z_query_keyexpr(query: &z_query_t) -> z_keyexpr_t {
+    let Some(query) = query.as_ref() else {
+        return z_keyexpr_t::null();
+    };
     query.key_expr().borrowing_clone().into()
 }
 
@@ -239,6 +307,9 @@ pub extern "C" fn z_query_keyexpr(query: &z_query_t) -> z_keyexpr_t {
 #[allow(clippy::missing_safety_doc)]
 #[no_mangle]
 pub extern "C" fn z_query_parameters(query: &z_query_t) -> z_bytes_t {
+    let Some(query) = query.as_ref() else {
+        return z_bytes_t::empty();
+    };
     let complement = query.parameters();
     z_bytes_t {
         start: complement.as_ptr(),
@@ -252,7 +323,7 @@ pub extern "C" fn z_query_parameters(query: &z_query_t) -> z_bytes_t {
 #[allow(clippy::missing_safety_doc)]
 #[no_mangle]
 pub unsafe extern "C" fn z_query_value(query: &z_query_t) -> z_value_t {
-    match query.value() {
+    match query.as_ref().and_then(|q| q.value()) {
         Some(value) => {
             #[allow(mutable_transmutes)]
             if let std::borrow::Cow::Owned(payload) = value.payload.contiguous() {

Cargo.toml
@@ -61,7 +61,7 @@ serde_yaml = "0.9.19"
 
 [lib]
 path="src/lib.rs"
-name = "zenohc"
+name = "zenohcd"
 crate-type = ["cdylib", "staticlib"]
 doctest = false
 

include/zenoh_concrete.h
@@ -37,7 +37,10 @@ typedef struct z_owned_session_t {
   size_t _0;
 } z_owned_session_t;
 /**
- * Structs received by a Queryable.
+ * Loaned variant of a Query received by a Queryable.
+ *
+ * Queries are atomically reference-counted, letting you extract them from the callback that handed them to you by cloning.
+ * `z_query_t`'s are valid as long as at least one corresponding `z_owned_query_t` exists, including the one owned by Zenoh until the callback returns.
  */
 typedef struct z_query_t {
   void *_0;

include/zenoh_macros.h
@@ -13,6 +13,7 @@
                   z_owned_encoding_t : z_encoding_loan,                        \
                   z_owned_hello_t : z_hello_loan,                              \
                   z_owned_str_t : z_str_loan,                                  \
+                  z_owned_query_t : z_query_loan,                              \
                   ze_owned_querying_subscriber_t : ze_querying_subscriber_loan \
             )(&x)
 
@@ -29,6 +30,7 @@
                   z_owned_reply_t * : z_reply_drop,                                   \
                   z_owned_hello_t * : z_hello_drop,                                   \
                   z_owned_str_t * : z_str_drop,                                       \
+                  z_owned_query_t * : z_query_drop,                                   \
                   z_owned_closure_sample_t * : z_closure_sample_drop,                 \
                   z_owned_closure_query_t * : z_closure_query_drop,                   \
                   z_owned_closure_reply_t * : z_closure_reply_drop,                   \
@@ -57,6 +59,7 @@
                   z_owned_reply_t * : z_reply_null,                                 \
                   z_owned_hello_t * : z_hello_null,                                 \
                   z_owned_str_t * : z_str_null,                                     \
+                  z_owned_query_t * : z_query_null,                                 \
                   z_owned_closure_sample_t * : z_closure_sample_null,               \
                   z_owned_closure_query_t * : z_closure_query_null,                 \
                   z_owned_closure_reply_t * : z_closure_reply_null,                 \
@@ -85,6 +88,7 @@
                   z_owned_encoding_t : z_encoding_check,                        \
                   z_owned_reply_t : z_reply_check,                              \
                   z_owned_hello_t : z_hello_check,                              \
+                  z_owned_query_t : z_query_check,                              \
                   z_owned_str_t : z_str_check,                                  \
                   zc_owned_payload_t : zc_payload_check,                        \
                   zc_owned_shmbuf_t : zc_shmbuf_check,                          \
@@ -134,6 +138,7 @@ template<> inline z_subscriber_t z_loan(const z_owned_subscriber_t& x) { return 
 template<> inline z_pull_subscriber_t z_loan(const z_owned_pull_subscriber_t& x) { return z_pull_subscriber_loan(&x); }
 template<> inline z_encoding_t z_loan(const z_owned_encoding_t& x) { return z_encoding_loan(&x); }
 template<> inline z_hello_t z_loan(const z_owned_hello_t& x) { return z_hello_loan(&x); }
+template<> inline z_query_t z_loan(const z_owned_query_t& x) { return z_query_loan(&x); }
 template<> inline const char* z_loan(const z_owned_str_t& x) { return z_str_loan(&x); }
 template<> inline ze_querying_subscriber_t z_loan(const ze_owned_querying_subscriber_t& x) { return ze_querying_subscriber_loan(&x); }
 
@@ -151,6 +156,7 @@ template<> struct zenoh_drop_type<z_owned_queryable_t> { typedef int8_t type; };
 template<> struct zenoh_drop_type<z_owned_encoding_t> { typedef void type; };
 template<> struct zenoh_drop_type<z_owned_reply_t> { typedef void type; };
 template<> struct zenoh_drop_type<z_owned_hello_t> { typedef void type; };
+template<> struct zenoh_drop_type<z_owned_query_t> { typedef void type; };
 template<> struct zenoh_drop_type<z_owned_str_t> { typedef void type; };
 template<> struct zenoh_drop_type<zc_owned_payload_t> { typedef void type; };
 template<> struct zenoh_drop_type<zc_owned_shmbuf_t> { typedef void type; };
@@ -177,6 +183,7 @@ template<> inline int8_t z_drop(z_owned_queryable_t* v) { return z_undeclare_que
 template<> inline void z_drop(z_owned_encoding_t* v) { z_encoding_drop(v); }
 template<> inline void z_drop(z_owned_reply_t* v) { z_reply_drop(v); }
 template<> inline void z_drop(z_owned_hello_t* v) { z_hello_drop(v); }
+template<> inline void z_drop(z_owned_query_t* v) { z_query_drop(v); }
 template<> inline void z_drop(z_owned_str_t* v) { z_str_drop(v); }
 template<> inline void z_drop(zc_owned_payload_t* v) { zc_payload_drop(v); }
 template<> inline void z_drop(zc_owned_shmbuf_t* v) { zc_shmbuf_drop(v); }
@@ -203,6 +210,7 @@ inline void z_null(z_owned_queryable_t& v) { v = z_queryable_null(); }
 inline void z_null(z_owned_encoding_t& v) { v = z_encoding_null(); }
 inline void z_null(z_owned_reply_t& v) { v = z_reply_null(); }
 inline void z_null(z_owned_hello_t& v) { v = z_hello_null(); }
+inline void z_null(z_owned_query_t& v) { v = z_query_null(); }
 inline void z_null(z_owned_str_t& v) { v = z_str_null(); }
 inline void z_null(zc_owned_payload_t& v) { v = zc_payload_null(); }
 inline void z_null(zc_owned_shmbuf_t& v) { v = zc_shmbuf_null(); }
@@ -234,6 +242,7 @@ inline bool z_check(const z_owned_queryable_t& v) { return z_queryable_check(&v)
 inline bool z_check(const z_owned_encoding_t& v) { return z_encoding_check(&v); }
 inline bool z_check(const z_owned_reply_t& v) { return z_reply_check(&v); }
 inline bool z_check(const z_owned_hello_t& v) { return z_hello_check(&v); }
+inline bool z_check(const z_owned_query_t& v) { return z_query_check(&v); }
 inline bool z_check(const z_owned_str_t& v) { return z_str_check(&v); }
 inline bool z_check(const zc_owned_liveliness_token_t& v) { return zc_liveliness_token_check(&v); }
 inline bool z_check(const ze_owned_publication_cache_t& v) { return ze_publication_cache_check(&v); }

commit f52f0761bb51fdd820cfacc51c335b06fbfc509a

Document that Queryable replies may be sent beyond the callback extent
The previous phrasing contradicts text in /docs/concepts.rst:

> [...] sometimes it's necessary to take ownership of the object passed
> to the callback for further processing.

> The only purpose of [take operations] is to allow user code to take
> ownership of the object passed to callbacks.

as well as the documentation for `z_query_clone`:

> Constructs a shallow copy of the query, allowing to keep it in an
> "open" state past the callback's return.

which seem to suggest that it is intended that the extent of validity of
query objects (and by extension, the expectation of successful reply
operations upon them) is tied to the lifetime of the content of the
object rather than the the lifetime of the enclosing stack frame.

This change describes the API guarantee for the `z_query_reply` family
by explicitly mentioning such a use case, resolving the conflict between
the specific and general docs for the reply and take APIs.

Fixes: 16cba0760b23 ("feat: owned queries now supported, allowing responses out of callback (#213)")
Fixes: 547ec402bfba ("take operation accepts mutable loaned reference for types which supports it (#923)")

include/zenoh_commons.h
@@ -5231,10 +5231,11 @@ struct z_loaned_bytes_t *z_query_payload_mut(struct z_loaned_query_t *this_);
 /**
  * Sends a reply to a query.
  *
- * This function must be called inside of a Queryable callback passing the
- * query received as parameters of the callback function. This function can
- * be called multiple times to send multiple replies to a query. The reply
- * will be considered complete when the Queryable callback returns.
+ * This function must be called passing the query received as parameters of the
+ * Queryable callback function. This function can be called multiple times to
+ * send multiple replies to a query. The reply will be considered complete when
+ * the query object is dropped, allowing `z_query_take_from_loaned()` to be
+ * used to produce replies outside of the Queryable callback.
  *
  * @param this_: The query to reply to.
  * @param key_expr: The key of this reply.
@@ -5251,10 +5252,11 @@ z_result_t z_query_reply(const struct z_loaned_query_t *this_,
 /**
  * Sends a delete reply to a query.
  *
- * This function must be called inside of a Queryable callback passing the
- * query received as parameters of the callback function. This function can
- * be called multiple times to send multiple replies to a query. The reply
- * will be considered complete when the Queryable callback returns.
+ * This function must be called passing the query received as parameters of the
+ * Queryable callback function. This function can be called multiple times to
+ * send multiple replies to a query. The reply will be considered complete when
+ * the query object is dropped, allowing `z_query_take_from_loaned()` to be
+ * used to produce replies outside of the Queryable callback.
  *
  * @param this_: The query to reply to.
  * @param key_expr: The key of this delete reply.
@@ -5273,10 +5275,11 @@ ZENOHC_API void z_query_reply_del_options_default(struct z_query_reply_del_optio
 /**
  * Sends a error reply to a query.
  *
- * This function must be called inside of a Queryable callback passing the
- * query received as parameters of the callback function. This function can
- * be called multiple times to send multiple replies to a query. The reply
- * will be considered complete when the Queryable callback returns.
+ * This function must be called passing the query received as parameters of the
+ * Queryable callback function. This function can be called multiple times to
+ * send multiple replies to a query. The reply will be considered complete when
+ * the query object is dropped, allowing `z_query_take_from_loaned()` to be
+ * used to produce replies outside of the Queryable callback.
  *
  * @param this_: The query to reply to.
  * @param payload: The payload carrying error message. Will be consumed.

src/queryable.rs
@@ -352,10 +352,11 @@ pub extern "C" fn z_queryable_id(queryable: &z_loaned_queryable_t) -> z_entity_g
 
 /// Sends a reply to a query.
 ///
-/// This function must be called inside of a Queryable callback passing the
-/// query received as parameters of the callback function. This function can
-/// be called multiple times to send multiple replies to a query. The reply
-/// will be considered complete when the Queryable callback returns.
+/// This function must be called passing the query received as parameters of the
+/// Queryable callback function. This function can be called multiple times to
+/// send multiple replies to a query. The reply will be considered complete when
+/// the query object is dropped, allowing `z_query_take_from_loaned()` to be
+/// used to produce replies outside of the Queryable callback.
 ///
 /// @param this_: The query to reply to.
 /// @param key_expr: The key of this reply.
@@ -401,10 +402,11 @@ pub extern "C" fn z_query_reply(
 
 /// Sends a error reply to a query.
 ///
-/// This function must be called inside of a Queryable callback passing the
-/// query received as parameters of the callback function. This function can
-/// be called multiple times to send multiple replies to a query. The reply
-/// will be considered complete when the Queryable callback returns.
+/// This function must be called passing the query received as parameters of the
+/// Queryable callback function. This function can be called multiple times to
+/// send multiple replies to a query. The reply will be considered complete when
+/// the query object is dropped, allowing `z_query_take_from_loaned()` to be
+/// used to produce replies outside of the Queryable callback.
 ///
 /// @param this_: The query to reply to.
 /// @param payload: The payload carrying error message. Will be consumed.
@@ -436,10 +438,11 @@ pub unsafe extern "C" fn z_query_reply_err(
 
 /// Sends a delete reply to a query.
 ///
-/// This function must be called inside of a Queryable callback passing the
-/// query received as parameters of the callback function. This function can
-/// be called multiple times to send multiple replies to a query. The reply
-/// will be considered complete when the Queryable callback returns.
+/// This function must be called passing the query received as parameters of the
+/// Queryable callback function. This function can be called multiple times to
+/// send multiple replies to a query. The reply will be considered complete when
+/// the query object is dropped, allowing `z_query_take_from_loaned()` to be
+/// used to produce replies outside of the Queryable callback.
 ///
 /// @param this_: The query to reply to.
 /// @param key_expr: The key of this delete reply.