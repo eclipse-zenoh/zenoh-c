eclipse-zenoh/zenoh-c

commit b5af25851344a6b4503aaf5e62edfa84a4f323e3

reintroduced queryables

include/zenoh_commons.h
@@ -102,6 +102,45 @@ typedef struct z_keyexpr_t {
   uintptr_t _padding[2];
 } z_keyexpr_t;
 /**
+ * An owned zenoh queryable.
+ *
+ * Like most `z_owned_X_t` types, you may obtain an instance of `z_X_t` by loaning it using `z_X_loan(&val)`.
+ * The `z_loan(val)` macro, available if your compiler supports C11's `_Generic`, is equivalent to writing `z_X_loan(&val)`.
+ *
+ * Like all `z_owned_X_t`, an instance will be destroyed by any function which takes a mutable pointer to said instance, as this implies the instance's inners were moved.
+ * To make this fact more obvious when reading your code, consider using `z_move(val)` instead of `&val` as the argument.
+ * After a move, `val` will still exist, but will no longer be valid. The destructors are double-drop-safe, but other functions will still trust that your `val` is valid.
+ *
+ * To check if `val` is still valid, you may use `z_X_check(&val)` or `z_check(val)` if your compiler supports `_Generic`, which will return `true` if `val` is valid.
+ */
+typedef struct z_owned_queryable_t {
+  uintptr_t _0[4];
+} z_owned_queryable_t;
+typedef struct z_query_t {
+  const void *_0;
+} z_query_t;
+/**
+ * A closure is a structure that contains all the elements for stateful, memory-leak-free callbacks:
+ * - `this` is a pointer to an arbitrary state.
+ * - `call` is the typical callback function. `this` will be passed as its last argument.
+ * - `drop` allows the callback's state to be freed.
+ *
+ * Closures are not guaranteed not to be called concurrently.
+ *
+ * We guarantee that:
+ * - `call` will never be called once `drop` has started.
+ * - `drop` will only be called ONCE, and AFTER EVERY `call` has ended.
+ * - The two previous guarantees imply that `call` and `drop` are never called concurrently.
+ */
+typedef struct z_owned_closure_query_t {
+  void *this_;
+  void (*call)(struct z_query_t, const void*);
+  void (*drop)(void*);
+} z_owned_closure_query_t;
+typedef struct z_queryable_opt {
+  bool complete;
+} z_queryable_opt;
+/**
  * The encoding of a payload, in a MIME-like format.
  *
  * For wire and matching efficiency, common MIME types are represented using an integer as `prefix`, and a `suffix` may be used to either provide more detail, or in combination with the `Empty` prefix to write arbitrary MIME types.
@@ -266,6 +305,22 @@ char *z_config_to_string(struct z_config_t config);
  * ease the retrieval of the concerned resource in the routing tables.
  */
 struct z_owned_keyexpr_t z_declare_keyexpr(struct z_session_t session, struct z_keyexpr_t keyexpr);
+/**
+ * Creates a Queryable for the given key expression.
+ *
+ * Parameters:
+ *     session: The zenoh session.
+ *     keyexpr: The key expression the Queryable will reply to.
+ *     callback: The callback function that will be called each time a matching query is received.
+ *     options: Options for the queryable.
+ *
+ * Returns:
+ *    The created :c:type:`z_owned_queryable_t` or null if the creation failed.
+ */
+struct z_owned_queryable_t z_declare_queryable(struct z_session_t session,
+                                               struct z_keyexpr_t keyexpr,
+                                               struct z_owned_closure_query_t *callback,
+                                               const struct z_queryable_opt *options);
 /**
  * Declare a subscriber for a given key expression.
  *
@@ -452,6 +507,20 @@ struct z_owned_session_t z_open(struct z_owned_config_t *config);
 /**
  * Calls the closure. Calling an uninitialized closure is a no-op.
  */
+void z_owned_closure_query_call(const struct z_owned_closure_query_t *closure,
+                                struct z_query_t sample);
+/**
+ * Drops the closure. Droping an uninitialized closure is a no-op.
+ */
+void z_owned_closure_query_drop(struct z_owned_closure_query_t *closure);
+/**
+ * Constructs a stateless closure from a pointer to function.
+ * The state pointer will always be a nullptr.
+ */
+struct z_owned_closure_query_t z_owned_closure_query_new_stateless(void (*call)(struct z_query_t, const void*));
+/**
+ * Calls the closure. Calling an uninitialized closure is a no-op.
+ */
 void z_owned_closure_sample_call(const struct z_owned_closure_sample_t *closure,
                                  const struct z_sample_t *sample);
 /**
@@ -485,6 +554,36 @@ int z_put(struct z_session_t session,
  */
 struct z_put_options_t z_put_options_default(void);
 /**
+ * Get a query's key by aliasing it.
+ */
+struct z_keyexpr_t z_query_keyexpr(struct z_query_t query);
+/**
+ * Send a reply to a query.
+ *
+ * This function must be called inside of a Queryable callback passing the
+ * query received as parameters of the callback function. This function can
+ * be called multiple times to send multiple replies to a query. The reply
+ * will be considered complete when the Queryable callback returns.
+ *
+ * Parameters:
+ *     query: The query to reply to.
+ *     key: The key of this reply.
+ *     payload: The value of this reply.
+ *     len: The length of the value of this reply.
+ */
+void z_query_reply(struct z_query_t query,
+                   struct z_keyexpr_t key,
+                   const uint8_t *payload,
+                   uintptr_t len);
+/**
+ * Get a query's value selector by aliasing it.
+ */
+struct z_bytes_t z_query_value_selector(struct z_query_t query);
+/**
+ * Returns `true` if `qable` is valid.
+ */
+bool z_queryable_check(const struct z_owned_queryable_t *qable);
+/**
  * Returns `true` if `session` is valid.
  */
 bool z_session_check(const struct z_owned_session_t *session);
@@ -504,6 +603,13 @@ struct z_subscriber_options_t z_subscriber_options_default(void);
  * Undeclare the key expression generated by a call to :c:func:`z_declare_keyexpr`.
  */
 void z_undeclare_keyexpr(struct z_session_t session, struct z_owned_keyexpr_t *keyexpr);
+/**
+ * Close a `z_owned_queryable_t`, droping it and invalidating it for doube-drop safety.
+ *
+ * Parameters:
+ *     qable: The :c:type:`z_owned_queryable_t` to close.
+ */
+void z_undeclare_queryable(struct z_owned_queryable_t *qable);
 void z_undeclare_subscriber(struct z_owned_subscriber_t *sub);
 /**
  * Returns the key expression's internal string by aliasing it.

src/queryable.rs
@@ -12,144 +12,174 @@
 //   ZettaScale Zenoh team, <zenoh@zettascale.tech>
 //
 
-// type Queryable = Option<Arc<Sender<bool>>>;
-// /// An owned zenoh queryable.
-// ///
-// /// Like most `z_owned_X_t` types, you may obtain an instance of `z_X_t` by loaning it using `z_X_loan(&val)`.
-// /// The `z_loan(val)` macro, available if your compiler supports C11's `_Generic`, is equivalent to writing `z_X_loan(&val)`.
-// ///
-// /// Like all `z_owned_X_t`, an instance will be destroyed by any function which takes a mutable pointer to said instance, as this implies the instance's inners were moved.
-// /// To make this fact more obvious when reading your code, consider using `z_move(val)` instead of `&val` as the argument.
-// /// After a move, `val` will still exist, but will no longer be valid. The destructors are double-drop-safe, but other functions will still trust that your `val` is valid.
-// ///
-// /// To check if `val` is still valid, you may use `z_X_check(&val)` or `z_check(val)` if your compiler supports `_Generic`, which will return `true` if `val` is valid.
-// #[repr(C)]
-// #[allow(non_camel_case_types)]
-// pub struct z_owned_queryable_t([usize; 1]);
-// impl AsRef<Queryable> for z_owned_queryable_t {
-//     fn as_ref(&self) -> &Queryable {
-//         unsafe { std::mem::transmute(self) }
-//     }
-// }
-// impl AsMut<Queryable> for z_owned_queryable_t {
-//     fn as_mut(&mut self) -> &mut Queryable {
-//         unsafe { std::mem::transmute(self) }
-//     }
-// }
-// #[allow(non_camel_case_types)]
-// pub struct z_query_t(Query);
+use std::ops::Deref;
 
-// /// Creates a Queryable for the given key expression.
-// ///
-// /// Parameters:
-// ///     session: The zenoh session.
-// ///     keyexpr: The key expression the Queryable will reply to.
-// ///     callback: The callback function that will be called each time a matching query is received.
-// ///     arg: A pointer that will be passed to the **callback** on each call.
-// ///
-// /// Returns:
-// ///    The created :c:type:`z_owned_queryable_t` or null if the creation failed.
-// #[allow(clippy::missing_safety_doc)]
-// #[no_mangle]
-// pub unsafe extern "C" fn z_queryable_new(
-//     session: z_session_t,
-//     keyexpr: z_keyexpr_t,
-//     callback: extern "C" fn(&z_query_t, *const c_void),
-//     arg: *mut c_void,
-// ) -> z_owned_queryable_t {
-//     let arg = Box::from_raw(arg);
-//     let (tx, rx) = bounded::<bool>(1);
-//     let r = z_owned_queryable_t(std::mem::transmute(Some(Arc::new(tx))));
-//     let queryable = session
-//         .as_ref()
-//         .as_ref()
-//         .expect(LOG_INVALID_SESSION)
-//         .queryable(keyexpr)
-//         .res()
-//         .unwrap();
-//     let mut queryable: zenoh::queryable::Queryable<'static> = std::mem::transmute(queryable);
+use libc::c_void;
+use zenoh::{
+    prelude::Sample,
+    queryable::{CallbackQueryable, Query},
+    Session,
+};
+use zenoh_util::core::SyncResolve;
 
-//     // Note: This is done to ensure that even if the call-back into C
-//     // does any blocking call we do not incour the risk of blocking
-//     // any of the task resolving futures.
-//     task::spawn_blocking(move || {
-//         task::block_on(async move {
-//             let arg = Box::into_raw(arg);
-//             loop {
-//                 select!(
-//                 query = queryable.receiver().next().fuse() => {
-//                   // This is a bit brutal but avoids an allocation and
-//                   // a copy that would be otherwise required to add the
-//                   // C string terminator. See the test_sub.c to find out how to deal
-//                   // with non null terminated strings.
-//                   let query = z_query_t(query.unwrap());
-//                   callback(&query, arg);
-//                 },
-//                 _ = rx.recv().fuse() => {
-//                     let _ = queryable.close().await;
-//                     return
-//                 })
-//             }
-//         })
-//     });
-//     r
-// }
+use crate::{
+    z_bytes_t, z_keyexpr_t, z_owned_closure_query_call, z_owned_closure_query_t, z_session_t,
+    LOG_INVALID_SESSION,
+};
 
-// /// Close a `z_owned_queryable_t`, droping it and invalidating it for doube-drop safety.
-// ///
-// /// Parameters:
-// ///     qable: The :c:type:`z_owned_queryable_t` to close.
-// #[allow(clippy::missing_safety_doc)]
-// #[no_mangle]
-// pub unsafe extern "C" fn z_queryable_close(qable: &mut z_owned_queryable_t) {
-//     let qable = qable.as_mut();
-//     match qable {
-//         Some(tx) => {
-//             let _ = async_std::task::block_on(tx.send(true));
-//             *qable = None;
-//         }
-//         None => (),
-//     }
-// }
+type Queryable = Option<CallbackQueryable<'static>>;
+/// An owned zenoh queryable.
+///
+/// Like most `z_owned_X_t` types, you may obtain an instance of `z_X_t` by loaning it using `z_X_loan(&val)`.
+/// The `z_loan(val)` macro, available if your compiler supports C11's `_Generic`, is equivalent to writing `z_X_loan(&val)`.
+///
+/// Like all `z_owned_X_t`, an instance will be destroyed by any function which takes a mutable pointer to said instance, as this implies the instance's inners were moved.
+/// To make this fact more obvious when reading your code, consider using `z_move(val)` instead of `&val` as the argument.
+/// After a move, `val` will still exist, but will no longer be valid. The destructors are double-drop-safe, but other functions will still trust that your `val` is valid.
+///
+/// To check if `val` is still valid, you may use `z_X_check(&val)` or `z_check(val)` if your compiler supports `_Generic`, which will return `true` if `val` is valid.
+#[repr(C)]
+#[allow(non_camel_case_types)]
+pub struct z_owned_queryable_t([usize; 4]);
+impl From<Queryable> for z_owned_queryable_t {
+    fn from(val: Queryable) -> Self {
+        unsafe { std::mem::transmute(val) }
+    }
+}
+impl AsRef<Queryable> for z_owned_queryable_t {
+    fn as_ref(&self) -> &Queryable {
+        unsafe { std::mem::transmute(self) }
+    }
+}
+impl AsMut<Queryable> for z_owned_queryable_t {
+    fn as_mut(&mut self) -> &mut Queryable {
+        unsafe { std::mem::transmute(self) }
+    }
+}
+#[allow(non_camel_case_types)]
+#[repr(C)]
+pub struct z_query_t(*const c_void);
+impl Deref for z_query_t {
+    type Target = Query;
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*(self.0 as *const _) }
+    }
+}
 
-// /// Returns `true` if `qable` is valid.
-// #[allow(clippy::missing_safety_doc)]
-// #[no_mangle]
-// pub unsafe extern "C" fn z_queryable_check(qable: &z_owned_queryable_t) -> bool {
-//     qable.as_ref().is_some()
-// }
+#[allow(non_camel_case_types)]
+#[repr(C)]
+pub struct z_queryable_opt {
+    pub complete: bool,
+}
 
-// /// Send a reply to a query.
-// ///
-// /// This function must be called inside of a Queryable callback passing the
-// /// query received as parameters of the callback function. This function can
-// /// be called multiple times to send multiple replies to a query. The reply
-// /// will be considered complete when the Queryable callback returns.
-// ///
-// /// Parameters:
-// ///     query: The query to reply to.
-// ///     key: The key of this reply.
-// ///     payload: The value of this reply.
-// ///     len: The length of the value of this reply.
-// #[allow(clippy::missing_safety_doc)]
-// #[no_mangle]
-// pub unsafe extern "C" fn z_send_reply(
-//     query: &z_query_t,
-//     key: *const c_char,
-//     payload: *const u8,
-//     len: c_uint,
-// ) {
-//     let name = CStr::from_ptr(key).to_str().unwrap();
-//     let s = Sample::new(
-//         name.to_string(),
-//         slice::from_raw_parts(payload as *const u8, len as usize),
-//     );
-//     query.0.replies_sender.send(s);
-// }
+/// Creates a Queryable for the given key expression.
+///
+/// Parameters:
+///     session: The zenoh session.
+///     keyexpr: The key expression the Queryable will reply to.
+///     callback: The callback function that will be called each time a matching query is received.
+///     options: Options for the queryable.
+///
+/// Returns:
+///    The created :c:type:`z_owned_queryable_t` or null if the creation failed.
+#[allow(clippy::missing_safety_doc)]
+#[no_mangle]
+pub unsafe extern "C" fn z_declare_queryable(
+    session: z_session_t,
+    keyexpr: z_keyexpr_t,
+    callback: &mut z_owned_closure_query_t,
+    options: Option<&z_queryable_opt>,
+) -> z_owned_queryable_t {
+    let mut closure = z_owned_closure_query_t::empty();
+    std::mem::swap(&mut closure, callback);
+    let session = match session.as_ref().as_ref() {
+        Some(s) => std::mem::transmute::<&Session, &'static Session>(s),
+        None => {
+            log::error!("{}", LOG_INVALID_SESSION);
+            return None.into();
+        }
+    };
+    let mut builder = session.declare_queryable(keyexpr);
+    if let Some(options) = options {
+        builder = builder.complete(options.complete);
+    }
+    builder
+        .callback(move |query| {
+            z_owned_closure_query_call(&closure, z_query_t(&query as *const _ as *const c_void))
+        })
+        .res_sync()
+        .map_err(|e| log::error!("{}", e))
+        .ok()
+        .into()
+}
 
-// // @TODO: replace when stable https://github.com/rust-lang/rust/issues/65816
-// #[inline]
-// pub(crate) fn vec_into_raw_parts<T>(v: Vec<T>) -> (*mut T, usize, usize) {
-//     let mut me = ManuallyDrop::new(v);
-//     (me.as_mut_ptr(), me.len(), me.capacity())
-// }
+/// Close a `z_owned_queryable_t`, droping it and invalidating it for doube-drop safety.
+///
+/// Parameters:
+///     qable: The :c:type:`z_owned_queryable_t` to close.
+#[allow(clippy::missing_safety_doc)]
+#[no_mangle]
+pub unsafe extern "C" fn z_undeclare_queryable(qable: &mut z_owned_queryable_t) {
+    if let Some(qable) = qable.as_mut().take() {
+        match qable.undeclare().res_sync() {
+            Ok(()) => {}
+            Err(e) => log::error!("{}", e),
+        }
+    }
+}
+
+/// Returns `true` if `qable` is valid.
+#[allow(clippy::missing_safety_doc)]
+#[no_mangle]
+pub unsafe extern "C" fn z_queryable_check(qable: &z_owned_queryable_t) -> bool {
+    qable.as_ref().is_some()
+}
+
+/// Send a reply to a query.
+///
+/// This function must be called inside of a Queryable callback passing the
+/// query received as parameters of the callback function. This function can
+/// be called multiple times to send multiple replies to a query. The reply
+/// will be considered complete when the Queryable callback returns.
+///
+/// Parameters:
+///     query: The query to reply to.
+///     key: The key of this reply.
+///     payload: The value of this reply.
+///     len: The length of the value of this reply.
+#[allow(clippy::missing_safety_doc)]
+#[no_mangle]
+pub unsafe extern "C" fn z_query_reply(
+    query: z_query_t,
+    key: z_keyexpr_t,
+    payload: *const u8,
+    len: usize,
+) {
+    if let Some(key) = &*key {
+        let s = Sample::new(
+            key.clone().into_owned(),
+            std::slice::from_raw_parts(payload as *const u8, len as usize),
+        );
+        if let Err(e) = query.reply(Ok(s)).res_sync() {
+            log::error!("{}", e)
+        }
+    }
+}
+
+/// Get a query's key by aliasing it.
+#[allow(clippy::missing_safety_doc)]
+#[no_mangle]
+pub unsafe extern "C" fn z_query_keyexpr(query: z_query_t) -> z_keyexpr_t {
+    query.key_expr().borrowing_clone().into()
+}
+
+/// Get a query's value selector by aliasing it.
+#[allow(clippy::missing_safety_doc)]
+#[no_mangle]
+pub unsafe extern "C" fn z_query_value_selector(query: z_query_t) -> z_bytes_t {
+    let complement = query.value_selector();
+    z_bytes_t {
+        start: complement.as_ptr(),
+        len: complement.len(),
+    }
+}

examples/z_queryable.c
@@ -10,81 +10,83 @@
 //
 // Contributors:
 //   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
-//
-// #include <stdio.h>
-// #include <string.h>
-// #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-// #include <windows.h>
-// #define sleep(x) Sleep(x * 1000)
-// #else
-// #include <unistd.h>
-// #endif
-// #include "zenoh.h"
-
-// char *expr = "demo/example/zenoh-c-queryable";
-// char *value = "Queryable from C!";
-
-// void query_handler(const z_query_t *query, const void *arg)
-// {
-//     z_bytes_t res = z_query_key_expr(query).suffix;
-//     z_bytes_t pred = z_query_predicate(query);
-//     printf(">> [Queryable ] Received Query '%.*s%.*s'\n", (int)res.len, res.start, (int)pred.len, pred.start);
-//     z_send_reply(query, expr, (const unsigned char *)value, strlen(value));
-// }
-
-// int main(int argc, char **argv)
-// {
-//     z_init_logger();
-
-//     if (argc > 1)
-//     {
-//         expr = argv[1];
-//     }
-//     z_owned_config_t config = z_config_default();
-//     if (argc > 2)
-//     {
-//         if (!z_config_insert_json(z_loan(config), Z_CONFIG_CONNECT_KEY, argv[2]))
-//         {
-//             printf("Couldn't insert value `%s` in configuration at `%s`. This is likely because `%s` expects a JSON-serialized list of strings\n", argv[2], Z_CONFIG_CONNECT_KEY, Z_CONFIG_CONNECT_KEY);
-//             exit(-1);
-//         }
-//     }
-
-//     printf("Opening session...\n");
-//     z_owned_session_t s = z_open(z_move(config));
-//     if (!z_check(s))
-//     {
-//         printf("Unable to open session!\n");
-//         exit(-1);
-//     }
-
-//     printf("Creating Queryable on '%s'...\n", expr);
-//     z_owned_queryable_t qable = z_queryable_new(z_loan(s), z_expr(expr), query_handler, NULL);
-//     if (!z_check(qable))
-//     {
-//         printf("Unable to create queryable.\n");
-//         exit(-1);
-//     }
-
-//     printf("Enter 'q' to quit...\n");
-//     char c = 0;
-//     while (c != 'q')
-//     {
-//         c = getchar();
-//         if (c == -1)
-//         {
-//             sleep(1);
-//         }
-//     }
-
-//     z_queryable_close(z_move(qable));
-//     z_close(z_move(s));
-//     return 0;
-// }
 
 #include <stdio.h>
+#include <string.h>
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#include <windows.h>
+#define sleep(x) Sleep(x * 1000)
+#else
+#include <unistd.h>
+#endif
+#include "zenoh.h"
+
+char *expr = "demo/example/zenoh-c-queryable";
+char *value = "Queryable from C!";
+z_keyexpr_t keyexpr;
+
+void query_handler(z_query_t query, const void *arg)
+{
+    char *res = z_keyexpr_to_string(z_query_keyexpr(query));
+    z_bytes_t pred = z_query_value_selector(query);
+    printf(">> [Queryable ] Received Query '%s%.*s'\n", res, (int)pred.len, pred.start);
+    z_query_reply(query, keyexpr, (const unsigned char *)value, strlen(value));
+    free(res);
+}
+
 int main(int argc, char **argv)
 {
-    printf("Unimplemented\n");
-    return -1;
+    z_init_logger();
+
+    if (argc > 1)
+    {
+        expr = argv[1];
+    }
+    z_owned_config_t config = z_config_default();
+    if (argc > 2)
+    {
+        if (!z_config_insert_json(z_loan(config), Z_CONFIG_CONNECT_KEY, argv[2]))
+        {
+            printf("Couldn't insert value `%s` in configuration at `%s`. This is likely because `%s` expects a JSON-serialized list of strings\n", argv[2], Z_CONFIG_CONNECT_KEY, Z_CONFIG_CONNECT_KEY);
+            exit(-1);
+        }
+    }
+
+    printf("Opening session...\n");
+    z_owned_session_t s = z_open(z_move(config));
+    if (!z_check(s))
+    {
+        printf("Unable to open session!\n");
+        exit(-1);
+    }
+    keyexpr = z_keyexpr(expr);
+    if (!z_check(keyexpr))
+    {
+        printf("%s is not a valid key expression", expr);
+        exit(-1);
+    }
+
+    printf("Creating Queryable on '%s'...\n", expr);
+    z_owned_closure_query_t callback = z_owned_closure_query_new_stateless(query_handler);
+    z_owned_queryable_t qable = z_declare_queryable(z_loan(s), keyexpr, z_move(callback), NULL);
+    if (!z_check(qable))
+    {
+        printf("Unable to create queryable.\n");
+        exit(-1);
+    }
+
+    printf("Enter 'q' to quit...\n");
+    char c = 0;
+    while (c != 'q')
+    {
+        c = getchar();
+        if (c == -1)
+        {
+            sleep(1);
+        }
+    }
+
+    z_undeclare_queryable(z_move(qable));
+    z_close(z_move(s));
+    return 0;
 }

examples/z_sub.c
@@ -20,11 +20,11 @@
 #endif
 #include "zenoh.h"
 
-void data_handler(const z_sample_t sample, void *arg)
+void data_handler(const z_sample_t *sample, const void *arg)
 {
-    char *keystr = z_keyexpr_to_string(sample.keyexpr);
+    char *keystr = z_keyexpr_to_string(sample->keyexpr);
     printf(">> [Subscriber] Received ('%s': '%.*s')\n",
-           keystr, (int)sample.payload.len, sample.payload.start);
+           keystr, (int)sample->payload.len, sample->payload.start);
     free(keystr);
 }
 

commit 6892c574ba52ebe59e9bc234b3141550362efc2d

Document queries as "finalized" and avoid implying callback-only
Writing that replies can be "complete" may be confusing, as Queryables
have an unrelated concept of "completeness."

The text about queries being callback-only is misleading as queries can
also be received via channels.

Fixes: b5af25851344 ("reintroduced queryables")
Fixes: a2209b729cda ("introduce query channels (#222)")
Suggested-by: DenisBiryukov91 <155981813+DenisBiryukov91@users.noreply.github.com>

include/zenoh_commons.h
@@ -5231,12 +5231,10 @@ struct z_loaned_bytes_t *z_query_payload_mut(struct z_loaned_query_t *this_);
 /**
  * Sends a reply to a query.
  *
- * This function must be called passing the query received as parameters of the
- * Queryable callback function. This function can be called multiple times to
- * send multiple replies to a query. The reply will be considered complete when
- * the query object has no live references, allowing `z_query_clone()` or
- * `z_query_take_from_loaned()` to be used to produce replies outside of the
- * Queryable callback.
+ * This function can be called multiple times to send multiple replies to a
+ * query. The query will be considered finalized when the query object has no
+ * live references, allowing `z_query_clone()` or `z_query_take_from_loaned()`
+ * to be used to produce replies outside of the Queryable callback.
  *
  * @param this_: The query to reply to.
  * @param key_expr: The key of this reply.
@@ -5253,12 +5251,10 @@ z_result_t z_query_reply(const struct z_loaned_query_t *this_,
 /**
  * Sends a delete reply to a query.
  *
- * This function must be called passing the query received as parameters of the
- * Queryable callback function. This function can be called multiple times to
- * send multiple replies to a query. The reply will be considered complete when
- * the query object has no live references, allowing `z_query_clone()` or
- * `z_query_take_from_loaned()` to be used to produce replies outside of the
- * Queryable callback.
+ * This function can be called multiple times to send multiple replies to a
+ * query. The query will be considered finalized when the query object has no
+ * live references, allowing `z_query_clone()` or `z_query_take_from_loaned()`
+ * to be used to produce replies outside of the Queryable callback.
  *
  * @param this_: The query to reply to.
  * @param key_expr: The key of this delete reply.
@@ -5277,12 +5273,10 @@ ZENOHC_API void z_query_reply_del_options_default(struct z_query_reply_del_optio
 /**
  * Sends a error reply to a query.
  *
- * This function must be called passing the query received as parameters of the
- * Queryable callback function. This function can be called multiple times to
- * send multiple replies to a query. The reply will be considered complete when
- * the query object has no live references, allowing `z_query_clone()` or
- * `z_query_take_from_loaned()` to be used to produce replies outside of the
- * Queryable callback.
+ * This function can be called multiple times to send multiple replies to a
+ * query. The query will be considered finalized when the query object has no
+ * live references, allowing `z_query_clone()` or `z_query_take_from_loaned()`
+ * to be used to produce replies outside of the Queryable callback.
  *
  * @param this_: The query to reply to.
  * @param payload: The payload carrying error message. Will be consumed.

src/queryable.rs
@@ -352,12 +352,10 @@ pub extern "C" fn z_queryable_id(queryable: &z_loaned_queryable_t) -> z_entity_g
 
 /// Sends a reply to a query.
 ///
-/// This function must be called passing the query received as parameters of the
-/// Queryable callback function. This function can be called multiple times to
-/// send multiple replies to a query. The reply will be considered complete when
-/// the query object has no live references, allowing `z_query_clone()` or
-/// `z_query_take_from_loaned()` to be used to produce replies outside of the
-/// Queryable callback.
+/// This function can be called multiple times to send multiple replies to a
+/// query. The query will be considered finalized when the query object has no
+/// live references, allowing `z_query_clone()` or `z_query_take_from_loaned()`
+/// to be used to produce replies outside of the Queryable callback.
 ///
 /// @param this_: The query to reply to.
 /// @param key_expr: The key of this reply.
@@ -403,12 +401,10 @@ pub extern "C" fn z_query_reply(
 
 /// Sends a error reply to a query.
 ///
-/// This function must be called passing the query received as parameters of the
-/// Queryable callback function. This function can be called multiple times to
-/// send multiple replies to a query. The reply will be considered complete when
-/// the query object has no live references, allowing `z_query_clone()` or
-/// `z_query_take_from_loaned()` to be used to produce replies outside of the
-/// Queryable callback.
+/// This function can be called multiple times to send multiple replies to a
+/// query. The query will be considered finalized when the query object has no
+/// live references, allowing `z_query_clone()` or `z_query_take_from_loaned()`
+/// to be used to produce replies outside of the Queryable callback.
 ///
 /// @param this_: The query to reply to.
 /// @param payload: The payload carrying error message. Will be consumed.
@@ -440,12 +436,10 @@ pub unsafe extern "C" fn z_query_reply_err(
 
 /// Sends a delete reply to a query.
 ///
-/// This function must be called passing the query received as parameters of the
-/// Queryable callback function. This function can be called multiple times to
-/// send multiple replies to a query. The reply will be considered complete when
-/// the query object has no live references, allowing `z_query_clone()` or
-/// `z_query_take_from_loaned()` to be used to produce replies outside of the
-/// Queryable callback.
+/// This function can be called multiple times to send multiple replies to a
+/// query. The query will be considered finalized when the query object has no
+/// live references, allowing `z_query_clone()` or `z_query_take_from_loaned()`
+/// to be used to produce replies outside of the Queryable callback.
 ///
 /// @param this_: The query to reply to.
 /// @param key_expr: The key of this delete reply.