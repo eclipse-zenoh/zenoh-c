9c862a1ef0d425186d8ae1d4f8bbc03e705842ab
Michael Ilyin <milyin@gmail.com>
2026-08-03 18:00:12 +0200

    rustfmt

--- buildrs/opaque_types_generator.rs
+++ buildrs/opaque_types_generator.rs
@@ -128,9 +128,7 @@ fn produce_opaque_types_data() -> (String, PathBuf) {
     // In this case the toolchain is ignored, since the toolchain is already baked into the cargo command.
     // Otherwise fall back to the default cargo command and toolchain.
     let mut command = match std::env::var("CARGO_COMMAND") {
-        Ok(cargo_command) if !cargo_command.is_empty() => {
-            std::process::Command::new(cargo_command)
-        },
+        Ok(cargo_command) if !cargo_command.is_empty() => std::process::Command::new(cargo_command),
         _ => {
             let mut command = std::process::Command::new("cargo");
             // Preserve the toolchain channel if one was specified (e.g., +1.75)
